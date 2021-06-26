7f31fb0aeb9dac0527eef1341d4279cb3cf67c2e
Sh4rkD <20006132@es.uveg.edu.mx>
2021-06-25 19:19:18 -0600

    Recortando codigo

--- PostWork-3/PostWork3.R
+++ PostWork-3/PostWork3.R
@@ -1,18 +1,19 @@
 df <- read.csv("https://github.com/sh4rkd/Equipo-16-R/raw/master/PostWork-2/csv/total.csv")
 datCasa<-df$FTHG
 datVis<-df$FTAG
-(FrecAbs<-table(datVis,datCasa))
+FrecAbs<-table(datVis,datCasa)
 sumaFrecAbs<-sum(FrecAbs)
-(FrecRel<-round(FrecAbs/sumaFrecAbs,4))
+
 #Pregunta 1.1
 (ProbCasa<-round(table(datCasa)/sumaFrecAbs,4))
 #Pregunta 1.2
 (ProbVis<-round(table(datVis)/sumaFrecAbs,4))
 #pregunta 1.3
-FrecRel
+(FrecRel<-round(FrecAbs/sumaFrecAbs,4))
+
+library(ggplot2)
 
 #Pregunta 2.1
-library(ggplot2)
 barplot(ProbCasa,xlab="Goles anotados", ylab="Probabilidad", 
         main = "Probabilidad de anotar goles por el equipo de la casa",
         ylim =c(0,0.5),
@@ -24,12 +25,11 @@ barplot(ProbVis,xlab="Goles anotados", ylab="Probabilidad",
         ylim =c(0,0.5),
         col = rgb(0.8,0.1,0.1,0.6))
 
-goles<-as.data.frame(FrecRel)
-
-ggplot(goles, aes(x=datCasa, y=datVis,fill = Freq))+ 
+#Pregunta 2.3
+ggplot(as.data.frame(FrecRel), aes(x=datCasa, y=datVis,fill = Freq))+ 
   geom_tile()+ 
   geom_text(aes(label=round(Freq,4)))+
-  scale_fill_gradient(low="white", high="blue") +
+  scale_fill_gradient(low="pink", high="blue") +
   labs(x="Numero de Goles de la Casa")+
   labs(y="Numero de Goles del Visitante")+
   labs(fill="Probabilidad")
